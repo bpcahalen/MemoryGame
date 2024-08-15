e2b2bd9612190230a800519bab3aee3cefcc47ad
bcahalen <bcahalen@gmail.com>
2024-08-15 15:44:00 -0400

    Finished the game, added Hard Core Mode, and finished off any sort of game logic needed for smooth game play

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,7 @@
 let cards = [];
 let firstCard, secondCard;
 let score = 0;
+let guessHCM = 0;
 let flipped = false;
 let boardFull = false;
 let hardCore = false;
@@ -8,7 +9,8 @@ const gameBoard = document.getElementById('board');
 
 
 document.getElementById('counter').textContent = score;
-// document.querySelector('.restart').addEventListener('click', resetCards);
+document.querySelector('.restart').addEventListener('click', resetCards);
+document.querySelector('.HCM').addEventListener('click', generateHCM);
 
 fetch("index.json")
     .then(response =>  response.json())
@@ -49,6 +51,19 @@ function generateCards(){
     }
 };
 
+function generateHCMCards(){
+    for(let card of cards){
+        const piece = document.createElement('div');
+        piece.classList.add('card');
+        piece.setAttribute('data-name', card.name);
+        piece.innerHTML = `
+        <div class="frontHCM"></div>
+        <div class="back"></div>`
+        gameBoard.appendChild(piece);
+        piece.addEventListener('click', flip);
+    }
+};  
+
 
 function flip(){
     if(boardFull){
@@ -67,25 +82,102 @@ function flip(){
     }
 
     secondCard = this;
-    match();
-    
+
+    if(hardCore){
+        matchHCM();
+    }else {
+        match();
+    }
 };
 
 function match(){
-    if(firstCard.dataset.name === secondCard.dataset.name){
+    if(firstCard.dataset.name !== secondCard.dataset.name){
+        setTimeout(() => {
+            firstCard.classList.remove('flip');
+            secondCard.classList.remove('flip');
+            resetBoard();
+        }, 750);
+    }else{
         score++
         document.getElementById('counter').textContent = score;
-        
-    }else{
-        firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip');
-    }
-
-
+        lockCards();
+        if(score == 8){
+            alert('Winner!');
+            return;
+        }else{
+        resetBoard();
+        }
+    }    
 };
 
-// generateHCM(){};
+function matchHCM(){
+    if(firstCard.dataset.name !== secondCard.dataset.name){
+        setTimeout(() => {
+            firstCard.classList.remove('flip');
+            secondCard.classList.remove('flip');
+            resetBoard();
+            guessHCM++;
+
+        if(guessHCM == 5){
+            if(confirm('Guess limit reached. You loose. Try again?')){
+                hardCore = true;
+            }else{
+                hardCore = false;
+            }
+            resetBoard();
+            shuffle();
+            guessHCM = 0;
+            score = 0;
+            document.getElementById('counter').textContent = score;
+            if(hardCore){
+                gameBoard.innerHTML = '';
+                generateHCMCards();
+            }else{
+                gameBoard.innerHTML = '';
+                generateCards();
+            }
+        }
+        }, 750);
+    }else{
+        score++
+        document.getElementById('counter').textContent = score;
+        lockCards();
+        guessHCM = 0;
+        if(score == 8){
+            alert('Winner!');
+            return;
+        }else{
+            resetBoard();
+        }
+    }    
+};
+
+function resetBoard(){
+    firstCard = null;
+    secondCard = null;
+    boardFull = false;
+}
+
+function lockCards(){
+    firstCard.removeEventListener('click', flip);
+    secondCard.removeEventListener('click', flip);
+}
+
+function generateHCM(){
+    alert('RULES: \n1.) All cards are blank. It will be a pure blind guess. \n2.) You will get 5 guesses to get a right answer. Every right answer resets your number of guesses.  \n3.) After 3 wrong guesses, you loose. \n4.) Have fun and Good Luck!')
+    resetBoard();
+    guessHCM = 0;
+    shuffle();
+    hardCore = true;
+    gameBoard.innerHTML = '';
+    generateHCMCards();
+};
 
 function resetCards(){
-   
+   resetBoard();
+   shuffle();
+   score = 0;
+   document.getElementById('counter').textContent = score;
+   gameBoard.innerHTML = '';
+   generateCards();
 };
